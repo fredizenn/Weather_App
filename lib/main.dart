92fae5b1b77d734b3564a41deaf567fcf76499af
Frederick Akuffo <kofiakuffoboafo@gmail.com>
2023-12-08 11:41:59 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/weather_screen.dart';
+
+void main() {
+  runApp(const WeatherApp());
+}
+
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(useMaterial3: true),
+      debugShowCheckedModeBanner: false,
+      home: const WeatherScreen(),
+    );
+  }
+}
